c51e18fca872140f8f4c32ebe2d20b6d77babcec
Virtu <maksimsarsov777@gmail.com>
2024-10-16 19:47:29 +0300

    Создали все таблицы для рейтинга

--- database/handlers/database_create.py
+++ database/handlers/database_create.py
@@ -54,7 +54,68 @@ async def create_payments_table():
         ''')
         await db.commit()
 
+async def create_users_rating():
+    database_path = 'database/data/users_rating.db'
+    os.makedirs(os.path.dirname(database_path), exist_ok=True)
+
+    async with aiosqlite.connect(database_path) as db:
+        await db.execute(''' 
+            CREATE TABLE IF NOT EXISTS users_rating (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                ID_user INTEGER,
+                rating TEXT
+            )
+        ''')
+        await db.commit()
+
+async def create_rating_criteria():
+    database_path = 'database/data/rating_criteria.db'
+    os.makedirs(os.path.dirname(database_path), exist_ok=True)
+
+    async with aiosqlite.connect(database_path) as db:
+        await db.execute(''' 
+            CREATE TABLE IF NOT EXISTS rating_criteria (
+                rating_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                action_type TEXT,
+                value_rating TEXT
+            )
+        ''')
+        
+        # Вставка данных с правильными названиями колонок
+        rating_data = [
+            ('advice', '0.5'),
+            ('material', '2'),
+            ('like_advice', '1'),
+            ('like_material', '1')
+        ]
+
+        await db.executemany('''
+            INSERT INTO rating_criteria (action_type, value_rating) 
+            VALUES (?, ?)
+        ''', rating_data)
+
+        await db.commit()
+
+async def create_rating_calculation():
+    database_path = 'database/data/rating_calculation.db'
+    os.makedirs(os.path.dirname(database_path), exist_ok=True)
+
+    async with aiosqlite.connect(database_path) as db:
+        await db.execute(''' 
+            CREATE TABLE IF NOT EXISTS rating_calculation (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                ID_user INTEGER,
+                rating_value TEXT,
+                action_type TEXT
+            )
+        ''')
+        await db.commit()
+
+
 async def create_all_databases():
     await create_users_table()
     await create_subscription_status_table()
     await create_payments_table()
+    await create_users_rating()
+    await create_rating_criteria()
+    await create_rating_calculation()
